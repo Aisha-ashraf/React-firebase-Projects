868387b79db05fe508185847d702e5644670d412
Aisha ashraf <mdashraf902785@gmail.com>
2024-05-11 01:01:25 +0530

    React-chat-room without google Authentication

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrkiqk5kgjf0GIClo2Xv33DkTNqfKkfLM",
+  authDomain: "react-chat-app-f9e3f.firebaseapp.com",
+  databaseURL: "https://react-chat-app-f9e3f-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-f9e3f",
+  storageBucket: "react-chat-app-f9e3f.appspot.com",
+  messagingSenderId: "1095133756918",
+  appId: "1:1095133756918:web:ce0adbe22ea2b9ff4b9bd7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
